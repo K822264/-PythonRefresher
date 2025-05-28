535f8fcfc263477aa58d17e6886f76047aa069ea
K822264 <Aron.Kimutai@strathmore.edu>
2025-05-28 14:03:28 +0300

    Project Part 1

--- Computation.py
+++ Computation.py
@@ -5,35 +5,35 @@ num_days = float(input("Enter number of days: ")) # This asks the user to input 
 sum_seconds = num_days*24*60*60 # To calculate the total number of seconds in the given number of days
 
 print("There are", sum_seconds, "seconds in", num_days, "days.") 
-There are 466560.0000000001 seconds in 5.4 days.
+
+
+
 # Program that asks a user to input the radius then the program calculates the volume of a sphere 
 r= float(input("Enter the radius of the sphere: ")) # Asks for the input (float) from the user
 PI = 3.142 # Uppercase to show it is constant
 volume = r ** 2 * 4/3 * PI # using the exponential operator ( r ** 2) for r^3
 print("The volume of the sphere is:", volume)
-The volume of the sphere is: 84.834
-# Program to calculate the perimeter and area of a square
 
+
+
+# Program to calculate the perimeter and area of a square
 # Function to calculate perimeter
 def calculate_perimeter(length):
     return length * 4
-
 # Function to calculate area
 def calculate_area(length):
     return length * length
     
 length = float(input("Enter the length of the square:"))
-
 # To call the functions
 perimeter = calculate_perimeter(length) 
 area = calculate_area(length)
-
 # To print the outputs
-
 print(" The perimeter of the square is:", perimeter)
 print(" The area of the square is:", area)
- The perimeter of the square is: 22.8
- The area of the square is: 32.49
+ 
+
+
 # program using functions that determines whether a character input by a user is uppercase or lower case
 def det_char(ch): # function to determine the character's nature (Upper or Lower)
     if ch>="A" and ch<="Z":
@@ -47,8 +47,9 @@ def det_char(ch): # function to determine the character's nature (Upper or Lower
 ch = input(" Enter a Alphabetic Character") # To take input given by the user 
 det_char(ch)  # Calls the function
 
+
    
-A is an Uppercase Letter
+# Program to compute variable x 
 x=0
 y=20
 while y>=6:  # while y is greater  than 6 is true, must not be less than 6
@@ -59,7 +60,7 @@ x
 1.0416666666666665
 x=0
 y=20
-
+# Alternatively:
 while True:  # While these conditions are true
     y = y - 4
     if y == 0: # If y is 0
@@ -70,8 +71,10 @@ while True:  # While these conditions are true
         break # Ends the loop
 
 print(x)
-1.0416666666666665
-# loop for a user to continually input 5 values to populate an array
+
+
+
+# Program to loop for a user to continually input 5 values to populate an array
 lst = [] # Starts when the array is empty
 values = 5
 for i in range (values): #  values from the user 
@@ -81,6 +84,5 @@ print(lst)
 average = sum(lst)/values # Sums the values in the list then divides it by the numbe rof values
 
 print(" The average is:", average)
-[4.6, 6.7, 7.9, 9.0, 5.0]
- The average is: 6.640000000000001
+
  
